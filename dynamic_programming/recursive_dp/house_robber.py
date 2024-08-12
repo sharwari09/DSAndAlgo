672fb4408490e41289e6f2c9868d57c92bcf53ef
Sharwari Phadnis <42701206+sharwari09@users.noreply.github.com>
2024-08-12 16:50:41 -0700

    Create house_robber.py

---
+++ dynamic_programming/recursive_dp/house_robber.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/house-robber/description/
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        return dp[-1]
+
+"""
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+"""
+
+"""
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
